68150045cff5bed0621393075dcf7e9d243f8cfd
Ankit kumar gupta <ankitgpt2345@gmail.com>
2026-03-24 05:58:44 +0000

    revision dataType and type conversion

---
+++ .devcontainer/basic/TypeConversion.js
@@ -0,0 +1,15 @@
+// type conversion
+
+let score="73"
+
+console.log(typeof score);  //string
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);  // number
+console.log(valueInNumber);   //73 **
+
+console.log(typeof score);   // string
+
+let score1="abcd "
+let valueInNumber1=Number(score1) 
+console.log(typeof valueInNumber1); // number  
+console.log(valueInNumber1);     //NaN   ** 
---
+++ .devcontainer/basic/dataType.js
@@ -0,0 +1,14 @@
+//dataType
+//1. primitive data type
+
+let name="ankit"        // string
+let age=23              // number range pow(2 53)
+let isLoggedIn=true;   // boolean               
+let pin;               // undefine , type=undefine, 
+let state=null;        // null   for standalone value, type=object
+                       // symbol = for uniquenes
+// console.log(typeof(age))   // type0f()= find type of dataType
+
+
+
+// 2. non primitive data type
